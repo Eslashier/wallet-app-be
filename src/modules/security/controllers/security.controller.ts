0c60dc6e7ed9ac245cd05a3154faa9255540ad99
Eslashier <anfelov@gmail.com>
2022-12-01 19:26:13 -0500

    reorganized folders and files

--- src/modules/security/controllers/security.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Post } from '@nestjs/common';
-import { ClientCreateDTO } from 'src/common/storage/dto/client.dto';
-
-
-@Controller('security')
-export class SecurityController {
-  @Post('signup')
-  signUp(newClientData: ClientCreateDTO): void {}
-}
--- src/modules/security/security.module.ts
+++ src/modules/security/security.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { SecurityController } from './controllers/security.controller';
-import { SecurityService } from './services/security.service';
 
 @Module({
-  controllers: [SecurityController],
-  providers: [SecurityService],
+  controllers: [],
+  providers: [],
 })
 export class SecurityModule {}
