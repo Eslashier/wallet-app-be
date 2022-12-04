8468066531383f7bbc68bdcbb68681a50601e011
Eslashier <anfelov@gmail.com>
2022-12-04 11:29:53 -0500

    added token-verification guard, if the request doesn't have bearer token now throws and 403 error

---
+++ src/modules/security/guards/token-verification.guard.ts
@@ -0,0 +1,63 @@
+import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Inject,
+} from '@nestjs/common';
+import {
+  JwtHeader,
+  SigningKeyCallback,
+  verify,
+  VerifyErrors,
+  VerifyOptions,
+} from 'jsonwebtoken';
+import * as jwksClient from 'jwks-rsa';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class TokenVerificationGuard implements CanActivate {
+  @Inject(ConfigService)
+  private readonly config: ConfigService;
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      const token = context
+        .switchToHttp()
+        .getRequest()
+        .get('authorization')
+        .replace('Bearer ', '');
+      console.log(token);
+      return this.getData(token);
+    } catch (err) {
+      return false;
+    }
+  }
+
+  private async getData(token: string): Promise<boolean> {
+    const client = jwksClient({
+      // jwksUri: this.config.get<string>('TOKEN_VERIFIER') || '',
+      jwksUri:
+        'https://dev-0gvplcuj1gtcwray.us.auth0.com/.well-known/jwks.json;',
+    });
+
+    const options: VerifyOptions = { algorithms: ['RS256'] };
+    const getKey = (header: JwtHeader, callback: SigningKeyCallback) => {
+      client.getSigningKey(header.kid, (err, key) => {
+        const signingKey = key?.getPublicKey();
+        callback(err, signingKey);
+      });
+    };
+
+    return new Promise((resolve) => {
+      verify(token, getKey, options, (err: VerifyErrors, decoded: any) => {
+        // console.log('decoded', decoded);
+        // console.log('err', err);
+        if (err) resolve(false);
+        resolve(true);
+      });
+    });
+  }
+}
